b9c3a137766e24dc8decc7772d271dfb67339461
Vincent Vlieghe <26386747+virtrixke@users.noreply.github.com>
2021-01-10 09:29:25 +0000

    Added go.mod and initial main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/virtrixke/golang-arch
+
+go 1.15
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First string
+}
 
 func main() {
-	fmt.Println("Hello world")
+	p1 := person{
+		First: "vincent",
+	}
+
+	p2 := person{
+		First: "barbara",
+	}
+
+	xp := []person{p1, p2}
+
+	bs, err := json.Marshal(xp)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(bs))
 }
