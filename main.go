4cb2dd39c70328a83d4e7ee19e35b1e1bd19bea0
Vincent Vlieghe <vincent.vlieghe@delen.be>
2021-01-16 18:58:45 +0100

    Added HMAC logic

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto"
+	"crypto/hmac"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key = []byte{}
+
 func main() {
+
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
 	pass := "P@ssw0rd"
 	hashedPass, err := hashPassword(pass)
 	if err != nil {
@@ -39,4 +47,27 @@ func comparePassword(password string, hashedPass []byte) error {
 		return fmt.Errorf("We got an error during compare pw: %w", err)
 	}
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(crypto.SHA512.New, key)
+
+	_, err := h.Write((msg))
+	if err != nil {
+		return nil, err
+	}
+
+	signature := h.Sum((nil))
+	return signature, nil
+}
+
+func checkSig(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSig %w", err)
+	}
+
+	same := hmac.Equal(newSig, sig)
+	return same, nil
+
 }
